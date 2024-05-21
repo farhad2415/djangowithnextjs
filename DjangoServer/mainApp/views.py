fd3a3c3693bf9328202690104d2c0fa2dec87936
Md Farhad Hossain <farhad.lep@gmail.com>
2024-05-21 15:57:54 +0600

    Added Typescript

--- DjangoServer/mainApp/views.py
+++ DjangoServer/mainApp/views.py
@@ -1,11 +1,16 @@
 from django.http import JsonResponse
+from django.shortcuts import render
 from .models import Product
+
 
 # Create your views here.
 
 
 def index(request):
 
-    products = Product.objects.all()
-    return JsonResponse({'products': list(products.values())})
+    products = Product.objects.order_by('id').reverse()
+
+    print(products)
+    # return data in html page
+    return render(request, 'index.html', {'products': products})  
 
